e87f88663cfb629683ca3400e3387e099f8ac79f
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-12 16:42:07 +0530

    ismultiple

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,10 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if m==0 and n==0:
+		return True
+	elif n!=0 and m%n==0:
+		return True
+	else:
+		return False
+# replace with your solution
