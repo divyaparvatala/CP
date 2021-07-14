0f838a0ff486bb0a08b4a8098c5c3e82b930e8f9
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-14 12:07:05 +0530

    pascaltriangle

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -4,9 +4,19 @@
 # Triangle where the triangle starts at row 0, and 
 # each row starts at column 0. If either row or col 
 # are not legal values, return None, instead of crashing. 
+import math
 
-
+def nCk(n,k):
+	nfactorial = math.factorial(n)
+	kfatorial = math.factorial(k)
+	nmkfactorial = math.factorial(n-k)
+	return nfactorial/(kfatorial*nmkfactorial)
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	if row>= 0 and col>=0:
+		if col<=row:
+			val = nCk(row,col)
+			return val
+		else:
+			return 0
