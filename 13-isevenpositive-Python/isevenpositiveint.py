15c271f91091818e418ba6a4bb4f3f650d9fff17
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-14 12:44:46 +0530

    isevenpositiveint

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -4,6 +4,17 @@
 # crashing if the value is not an integer. So, isevenpositiveint("yikes!") returns False (rather
 # than crashing), and isevenpositiveint(123456) returns True.
 
+def isNumeric(x):
+	res = x.isnumeric()
+	return res
+
 def isevenpositiveint(x):
+	if isNumeric(str(x)) == True:
+		if int(x)%2 ==0:
+			return True
+		else:
+			return False
+	else:
+		return False
 	# your code goes here
-	pass
+	# pass
