41ba91a62483d675b5ea3b81cb63489fb991d871
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-16 00:02:43 +0530

    mostfrequent

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -3,5 +3,31 @@
 # that occurs most frequently in it, with ties going to the smaller digit.
 
 def mostfrequentdigit(n):
+	nlist = [int(i) for i in str(n)]
+	print(nlist)
+	freq = nlist.count(nlist[0])
+	digit = 0
+	equallist=[]
+	for i in nlist:
+		# print(i)
+		freq_ = nlist.count(i)
+		print('first',i,freq_,freq)
+		if freq_>freq:
+			freq = freq_
+			# digit = i
+			equallist.clear()
+			equallist.append(i)
+			print('second',i,freq_,freq)
+		elif freq_==freq:
+			equallist.append(i)
+			# digit = min(i,freq_)
+			# print('digit',digit)
+	return min(equallist)
+
+
+print(mostfrequentdigit(5231123123123))
+
+
+
 	# your code goes here
-	pass
+	# pass
