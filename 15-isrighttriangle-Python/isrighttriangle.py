b6f599afd3133c3dd6478dc2a2f9190aaf616877
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-15 23:32:42 +0530

    isright

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,11 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	m1 = (y2-y1)/(x2-x1)
+	m2 = (y3-y2)/(x3-x2)
+	m3 = (y1-y3)/(x1-x3)
+	if m1*m2==-1 or m2*m3==-1 or m3*m1==-1:
+		return True
+	else:
+		return False
+	# pass
