81107afde7f163262901ee3751e137666c021dd5
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-19 17:29:16 +0530

    applycaesarcipher_

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -10,32 +10,56 @@
 # assert(applyCaesarCipher("zodiac", -2) == "xmbgya")
 
 
+# def fun_applycaesarcipher(msg, shift):
+# 	lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# 	upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+# 	msglist = [i for i in msg]
+# 	shiftmsglist = []
+# 	shiftmsg = ''
+# 	for i in msg:
+# 		if i in lower:
+# 			print(i)
+# 			i = lower[(lower.index(i)+shift)%26]
+# 			print(i)
+# 			shiftmsglist.append(i)
+# 		elif i in upper:
+# 			print(i)
+# 			i = upper[(upper.index(i)+shift)%26]
+# 			print(i)
+# 			shiftmsglist.append(i)
+# 		else:
+# 			print(i)
+# 			shiftmsglist.append(i)
+
+# 	return "".join(shiftmsglist)
+# print(fun_applycaesarcipher("We Attack", 1))
+
 def fun_applycaesarcipher(msg, shift):
-	lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-	upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 	msglist = [i for i in msg]
-	shiftmsglist = []
+	# print(msglist)
+	ordlist = [ord(i) for i in msg]
 	shiftmsg = ''
-	for i in msg:
-		if i in lower:
-			print(i)
-			i = lower[(lower.index(i)+shift)%26]
-			print(i)
-			shiftmsglist.append(i)
-		elif i in upper:
-			print(i)
-			i = upper[(upper.index(i)+shift)%26]
-			print(i)
-			shiftmsglist.append(i)
+	newilist = []
+	newi = 0
+	for i in msglist:
+		if i.isalpha():
+			if i.isupper():
+				newi = ((ord(i)+shift-65)%26)+ 65
+				print(ord(i),newi)
+				newilist.append(chr(newi))
+			elif i.islower():
+				newi = ((ord(i)+shift-97)%26) + 97
+				print(ord(i),newi)
+				newilist.append(chr(newi))
 		else:
-			print(i)
-			shiftmsglist.append(i)
-	for i in shiftmsglist:
-		shiftmsg += i
-
-
-	return shiftmsg
+			newilist.append(i)
+	
+	return "".join(newilist)
+	
 print(fun_applycaesarcipher("We Attack", 1))
+
+
+
 
 
 
