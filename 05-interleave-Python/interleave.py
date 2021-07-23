0f94d89dd47a00a9ff2ba5a2d2e1f61050e0a2e2
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-07-23 09:00:08 +0530

    rec

---
+++ 05-interleave-Python/interleave.py
@@ -0,0 +1,18 @@
+# Write the function interleave(s1, s2) that takes two strings, s1 and s2, 
+# and interleaves their characters starting with the first character in s1. 
+# For example, interleave('pto', 'yhn') would return the string "python". 
+# If one string is longer than the other, concatenate the rest of the remaining 
+# string onto the end of the new string. For example ('a#', 'cD!f2') would return 
+# the string "ac#D!f2". Assume that both s1 and s2 will always be strings.
+
+
+
+def fun_interleave(s1,s2):
+	res = ""
+	s1len = len(s1)
+	s2len = len(s2)
+	for i in s1:
+		if s1len !=0:
+			
+	return ""
+	
