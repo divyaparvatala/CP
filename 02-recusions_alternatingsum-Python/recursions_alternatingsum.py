adcc89c67bfda81662ee8dac27ab14ec511b6171
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-08-01 22:46:17 +0530

    rec

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -5,4 +5,23 @@
 
 
 def fun_recursions_alternatingsum(l): 
-	return 0
+	if len(l)==0:
+		return 0
+	elif len(l)==1:
+		# print('length :   ', len(l))
+		# print(l)
+		return l[0]
+	else:
+		# if len(l)%2==0:
+		# # 	print('list: ',l[1:])
+		# # 	print('first element :', l[0])
+		# # 	print('length :   ', len(l))
+		# 	return l[0] - fun_recursions_alternatingsum(l[1:])
+		# elif len(l)%2==1:
+		# # 	print('list: ',l[1:])
+		# # 	print('first element :', l[0])
+		# # 	print('length :   ', len(l))
+		print('0, [1:], [2:]',l[0], fun_recursions_alternatingsum(l[1:]), fun_recursions_alternatingsum(l[2:]))
+		print('res',l[0] - fun_recursions_alternatingsum(l[1:]) + fun_recursions_alternatingsum(l[2:]))
+		return l[0] - fun_recursions_alternatingsum(l[1:]) + fun_recursions_alternatingsum(l[2:])
+print(fun_recursions_alternatingsum([1,2,3,4]))
