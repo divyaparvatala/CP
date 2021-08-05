cd2134009b0c801f967b0b5b928ac4a48c81891e
divyaparvatala <71515542+divyaparvatala@users.noreply.github.com>
2021-08-05 23:58:57 +0530

    set

--- set.py
+++ set.py
@@ -9,4 +9,21 @@
 
 def limitedPowerSet(n, k):
     # Your code goes here...
-    pass
+    # pass
+    res = [{}]
+    for i in range(1,n+1):
+        x = {i}
+        res.append(x)
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if len(res) == k:
+                return res
+            else:
+                if j!=i:
+                    y = {i,j}
+                    res.append(y)
+
+assert(limitedPowerSet(5,7)==[{}, {1}, {2}, {3}, {4}, {5}, {1, 2}])
+assert(limitedPowerSet(6,7)==[{}, {1}, {2}, {3}, {4}, {5}, {6}])
+assert(limitedPowerSet(6,10)==[{}, {1}, {2}, {3}, {4}, {5}, {6}, {1, 2}, {1, 3}, {1, 4}])
+print('Test cases passed')
